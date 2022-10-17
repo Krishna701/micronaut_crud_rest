7d584e3e7017da544f9ebe1e594fa7bf4e175f93
Krishna Rijal <Krishna701>
2022-10-17 03:55:37 +0000

    Initial commit

---
+++ src/main/java/micronaut_crud_rest/Application.java
@@ -0,0 +1,9 @@
+package micronaut_crud_rest;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
